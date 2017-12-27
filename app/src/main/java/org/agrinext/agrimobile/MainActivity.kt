2321516f32c2d0b7bbae7b61fb483c134691f0f0
Revant Nandgaonkar <revant.one@gmail.com>
2017-12-27 16:39:23 +0530

    First Commit

---
+++ app/src/main/java/org/agrinext/agrimobile/MainActivity.kt
@@ -0,0 +1,93 @@
+package org.agrinext.agrimobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.app_bar_main.*
+import org.jetbrains.anko.share
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_market -> {
+                toast("Market Clicked")
+            }
+            R.id.nav_sellers -> {
+                toast("Sellers Clicked")
+            }
+            R.id.nav_chats -> {
+                toast("Chats Clicked")
+            }
+            R.id.nav_my_profile -> {
+                startActivity(Intent(this, UserProfile::class.java))
+            }
+            R.id.nav_my_produce -> {
+                toast("My Produce Clicked")
+            }
+            R.id.nav_invite -> {
+                toast("Invite Clicked")
+            }
+            R.id.nav_locations -> {
+                share("https://agrinext.org")
+            }
+            R.id.nav_items -> {
+                toast("Items Clicked")
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
+}
---
+++ app/src/main/java/org/agrinext/agrimobile/UserProfile.kt
@@ -0,0 +1,16 @@
+package org.agrinext.agrimobile
+
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+
+
+class UserProfile : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}

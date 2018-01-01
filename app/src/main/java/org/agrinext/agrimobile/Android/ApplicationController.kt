3a6448b398939c71a78b18522e33c4604ab4e215
Revant Nandgaonkar <revant.one@gmail.com>
2018-01-01 14:08:20 +0530

    Volley request and serial request

--- app/src/main/java/org/agrinext/agrimobile/Android/ApplicationController.kt
+++ app/src/main/java/org/agrinext/agrimobile/Android/ApplicationController.kt
@@ -1,20 +1,60 @@
 package org.agrinext.agrimobile.Android
 
 import android.app.Application
-import org.jetbrains.anko.activityManager
-
+import android.content.Context
+import com.android.volley.Network
+import com.android.volley.toolbox.Volley
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.DiskBasedCache
+import com.android.volley.toolbox.BasicNetwork
+import com.android.volley.toolbox.HurlStack
+import com.android.volley.toolbox.HttpStack
 
 /**
  * Created by revant on 31/12/17.
  */
 class ApplicationController : Application() {
     internal var activityVisible = false
+    lateinit var mRequestQueue: RequestQueue
+    lateinit var mSerialRequestQueue: RequestQueue
+    var MAX_SERIAL_THREAD_POOL_SIZE = 1
+    val MAX_CACHE_SIZE = 2 * 1024 * 1024 //2 MB
 
     override fun onCreate() {
         super.onCreate()
         instance = this
     }
 
+    fun getRequestQueue(): RequestQueue {
+        // lazy initialize the request queue, the queue instance will be
+        // created when it is accessed for the first time
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(applicationContext)
+        }
+
+        return mRequestQueue
+    }
+
+    fun getSerialRequestQueue(): RequestQueue {
+        if (mSerialRequestQueue == null) {
+            mSerialRequestQueue = prepareSerialRequestQueue(applicationContext)
+            mSerialRequestQueue.start()
+        }
+        return mSerialRequestQueue
+    }
+
+    fun prepareSerialRequestQueue(context: Context): RequestQueue {
+        val cache = DiskBasedCache(context.getCacheDir(), MAX_CACHE_SIZE)
+        val network = getNetwork()
+        return RequestQueue(cache, network, MAX_SERIAL_THREAD_POOL_SIZE)
+    }
+
+    fun getNetwork(): Network {
+        val stack: HttpStack
+        val userAgent = "volley/0"
+        stack = HurlStack()
+        return BasicNetwork(stack)
+    }
     fun setConnectivityListener(listener: ConnectivityReceiver.ConnectivityReceiverListener) {
         ConnectivityReceiver.connectivityReceiverListener = listener
     }

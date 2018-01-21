1be2ac7063b6b71a5c275dc71b28b1e27540eddb
Revant Nandgaonkar <revant.one@gmail.com>
2018-01-21 12:52:47 +0530

    Filters Activity field and expression added

--- app/src/main/java/org/agrinext/agrimobile/Activities/FiltersActivity.kt
+++ app/src/main/java/org/agrinext/agrimobile/Activities/FiltersActivity.kt
@@ -25,18 +25,18 @@ class FiltersActivity : BaseCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_filters)
 
+        //set doctype and meta
+        if(intent.hasExtra(DOCTYPE)){
+            setupDocType(intent.getStringExtra(DOCTYPE))
+        }
+
         mRecyclerView = findViewById(R.id.filter_recycler_view)
 
         val mLayoutManager = LinearLayoutManager(this)
         mRecyclerView.setLayoutManager(mLayoutManager)
 
-        recyclerAdapter = FilterViewAdapter(recyclerModels)
+        recyclerAdapter = FilterViewAdapter(recyclerModels, docMeta!!)
         mRecyclerView.adapter = recyclerAdapter
-
-        //set doctype and meta
-        if(intent.hasExtra(DOCTYPE)){
-            setupDocType(intent.getStringExtra(DOCTYPE))
-        }
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -44,15 +44,13 @@ class FiltersActivity : BaseCompatActivity() {
 
         bAddFilter.onClick {
             var jsonArray = JSONArray()
-            val fields = docMeta?.getJSONArray("fields")!!
-
-            for (i in 0 until fields.length() - 1){
-                if(fields.getJSONObject(i).has("label")){
-                    jsonArray.put(fields.getJSONObject(i).getString("label"))
-                }
-            }
             recyclerModels.put(jsonArray)
             recyclerAdapter!!.notifyItemInserted(recyclerModels.length() - 1)
+        }
+
+        bSetFilter.onClick {
+            Log.d("bSetFilters", recyclerModels.toString())
+            toast("Set Filters!")
         }
     }
 }
--- app/src/main/java/org/agrinext/agrimobile/Android/FilterViewAdapter.kt
+++ app/src/main/java/org/agrinext/agrimobile/Android/FilterViewAdapter.kt
@@ -1,9 +1,7 @@
 package org.agrinext.agrimobile.Android
 
 import android.R
-import android.content.Context
 import android.support.v7.widget.RecyclerView
-import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
@@ -17,21 +15,39 @@ import java.util.ArrayList
  * Created by revant on 28/12/17.
  */
 
-class FilterViewAdapter(var filterList:JSONArray): RecyclerView.Adapter<FilterViewAdapter.ViewHolder>() {
-    val immutableFilterlist = filterList
+class FilterViewAdapter(filterList:JSONArray): RecyclerView.Adapter<FilterViewAdapter.ViewHolder>() {
+    var immutableFilterlist = JSONArray()
+    var filterList = JSONArray()
+    var docMeta = JSONObject()
+    init {
+        immutableFilterlist = filterList
+        this.filterList = filterList
+    }
+
+    constructor(filterList: JSONArray, docMeta:JSONObject) : this(filterList) {
+        immutableFilterlist = filterList
+        this.filterList = filterList
+        this.docMeta = docMeta
+    }
+
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-
+        val context = itemView.context
         // Get list_item (and other fields) from ListItemUI
         val spinner: Spinner = itemView.find(FiltersItemUI.Companion.Ids.docFieldSpinner)
         // Bind values to name and other fields above
-        fun bind(jsonArray: JSONArray?) {
-            var list = ArrayList<String>()
-
-            for(i in 0 until jsonArray?.length()!! - 1){
-                list.add(jsonArray.getString(i))
+        fun bind(filtersArray: JSONArray?, fieldsArray: JSONArray) {
+            var list = ArrayList<String>().apply {
+                add(context.resources.getString(org.agrinext.agrimobile.R.string.sort_name))
+                add(context.resources.getString(org.agrinext.agrimobile.R.string.last_modified_on))
+                add(context.resources.getString(org.agrinext.agrimobile.R.string.created_on))
+                add(context.resources.getString(org.agrinext.agrimobile.R.string.most_used))
             }
-            val spinnerAdapter = ArrayAdapter<String>(itemView.context, R.layout.simple_spinner_item, list)
+
+            for(i in 0 until fieldsArray?.length()!! - 1){
+                list.add(fieldsArray.getString(i))
+            }
+            val spinnerAdapter = ArrayAdapter<String>(itemView.context, R.layout.simple_list_item_1, list)
             spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             spinner.adapter = spinnerAdapter
         }
@@ -51,7 +67,16 @@ class FilterViewAdapter(var filterList:JSONArray): RecyclerView.Adapter<FilterVi
         while(filterList.length() < p){
             p -= filterList.length()
         }
-        val jsonArray = filterList.getJSONArray(p)
-        holder!!.bind(jsonArray)
+
+        val fieldsArray = JSONArray()
+        val fields = docMeta?.getJSONArray("fields")!!
+
+        for (i in 0 until fields.length() - 1){
+            if(fields.getJSONObject(i).has("label")){
+                fieldsArray.put(fields.getJSONObject(i).getString("label"))
+            }
+        }
+        val filtersArray = filterList.getJSONArray(p)
+        holder!!.bind(filtersArray, fieldsArray)
     }
 }
